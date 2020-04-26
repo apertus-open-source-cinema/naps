206be54486ad26d640d559a7537ffaab095a8026
Jaro Habiger <jarohabiger@googlemail.com>
2020-04-26 17:01:09 +0200

    add basic simulation test to reg

--- src/modules/axi/test_simulate_axi_slaves.py
+++ src/modules/axi/test_simulate_axi_slaves.py
@@ -9,18 +9,21 @@ class TestAxiSlave(FHDLTestCase):
     def check_read_transaction(self, address, slave):
         sim = Simulator(slave)
 
-        def timeout(expr, timeout=20):
+        def timeout(expr, timeout=100):
             for i in range(timeout):
                 if (yield expr):
-                    break
+                    return
+                else:
+                    yield
             raise TimeoutError()
 
         def process():
             yield slave.axi.read_address.value.eq(address)
             yield slave.axi.read_address.valid.eq(1)
-            while not (yield slave.axi.read_address.ready):
-                yield
-            # yield from timeout(slave.axi.read_data.valid)
+            yield from timeout(slave.axi.read_address.ready)
+            yield from timeout(slave.axi.read_data.valid)
+            yield slave.axi.read_data.ready.eq(1)
+            yield Passive()
 
 
         sim.add_clock(1e-6)
@@ -29,4 +32,4 @@ class TestAxiSlave(FHDLTestCase):
             sim.run_until(10e-6, run_passive=True)
 
     def test_read_axil_reg(self):
-        self.check_read_transaction(0x1231456, AxiLiteReg(width=32, base_address=0x123456, name="test"))
+        self.check_read_transaction(0x123456, AxiLiteReg(width=32, base_address=0x123456, name="test"))

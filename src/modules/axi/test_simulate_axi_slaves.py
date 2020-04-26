341b7d2d60b4a0c6fc2b257d426baa8b3807cff5
Jaro Habiger <jarohabiger@googlemail.com>
2020-04-27 00:18:39 +0200

    simulate interconnect for reads

--- src/modules/axi/test_simulate_axi_slaves.py
+++ src/modules/axi/test_simulate_axi_slaves.py
@@ -2,34 +2,52 @@ from nmigen import *
 from nmigen.test.utils import FHDLTestCase
 from nmigen.back.pysim import Simulator, Tick, Settle, Passive
 
+from modules.axi.axi import AxiInterface
 from modules.axi.axil_reg import AxiLiteReg
+from modules.axi.interconnect import AxiInterconnect
 
 
 class TestAxiSlave(FHDLTestCase):
-    def check_read_transaction(self, address, slave):
-        sim = Simulator(slave)
+    def check_read_transaction(self, addresses, dut, filename="test"):
+        sim = Simulator(dut)
+        axi = dut.axi
 
-        def timeout(expr, timeout=100):
+        def timeout(expr, timeout=10):
             for i in range(timeout):
+                yield
                 if (yield expr):
                     return
-                else:
-                    yield
             raise TimeoutError()
 
         def process():
-            yield slave.axi.read_address.value.eq(address)
-            yield slave.axi.read_address.valid.eq(1)
-            yield from timeout(slave.axi.read_address.ready)
-            yield from timeout(slave.axi.read_data.valid)
-            yield slave.axi.read_data.ready.eq(1)
-            yield Passive()
+            for addr in addresses:
+                yield axi.read_address.value.eq(addr)
+                yield axi.read_address.valid.eq(1)
+                yield from timeout(axi.read_address.ready)
+                yield axi.read_address.valid.eq(0)
+                yield from timeout(axi.read_data.valid)
+                yield axi.read_data.ready.eq(1)
+
 
 
         sim.add_clock(1e-6)
         sim.add_sync_process(process)
-        with sim.write_vcd("test.vcd", "test.gtkw",  traces=(slave.axi._rhs_signals())):
-            sim.run_until(10e-6, run_passive=True)
+        with sim.write_vcd("{}.vcd".format(filename), "{}.gtkw".format(filename), traces=(dut.axi._rhs_signals())):
+            sim.run()
 
-    def test_read_axil_reg(self):
-        self.check_read_transaction(0x123456, AxiLiteReg(width=32, base_address=0x123456, name="test"))
+    def test_read_axil_reg(self, addr=0x123456):
+        dut = AxiLiteReg(width=32, base_address=addr, name="test")
+        self.check_read_transaction((addr,), dut, filename="test_read_axil_reg")
+
+    def test_read_interconnect_axil_reg(self, addr=0x123456, num_regs=10):
+        addrs = [addr + i for i in range(num_regs)]
+        m = Module()
+
+        m.axi = AxiInterface(addr_bits=32, data_bits=32, master=True, lite=True)
+        interconnect = m.submodules.interconnect = AxiInterconnect(m.axi)
+        for addr in addrs:
+            axil_reg = AxiLiteReg(width=32, base_address=addr, name="test")
+            m.d.comb += interconnect.get_port().connect_slave(axil_reg.axi)
+            m.submodules += axil_reg
+
+        self.check_read_transaction(addrs, dut=m, filename="test_read_interconnect_axil_reg")

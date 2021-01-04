16855800d7618a7c194df695a730125864267d98
Jaro Habiger <jarohabiger@googlemail.com>
2021-01-04 01:47:38 +0100

    run wavelet decoder as part of wavelet encoder

--- src/lib/video/wavelet_decompressor.py
+++ src/lib/video/wavelet_decompressor.py
@@ -16,8 +16,8 @@ def inverse_wavelet(lf_part, hf_part, pad_width=0):
 
     x, y = lf_part.shape
     res = np.zeros((x * 2, y), dtype=ty)
-    res[0::2] = np.round(((np.roll(lf_part, -1, 0) - np.roll(lf_part, +1, 0) + 4) // 8 + hf_part + lf_part) / 2)
-    res[1::2] = np.round(((-np.roll(lf_part, -1, 0) + np.roll(lf_part, +1, 0) + 4) // 8 - hf_part + lf_part) / 2)
+    res[0::2] = (((np.roll(lf_part, +1, 0) - np.roll(lf_part, -1, 0) + 4) >> 3) + hf_part + lf_part) >> 1
+    res[1::2] = (((-np.roll(lf_part, +1, 0) + np.roll(lf_part, -1, 0) + 4) >> 3) - hf_part + lf_part) >> 1
 
     if pad_width > 0:
         return res[2 * pad_width:-2 * pad_width, pad_width:-pad_width]
@@ -25,16 +25,7 @@ def inverse_wavelet(lf_part, hf_part, pad_width=0):
         return res
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f'usage:\n{sys.argv[0]} <input_dir>')
-        exit(1)
-    _, input_dir = sys.argv
-
-    a = (np.array(Image.open(join(input_dir, "0.png"))).astype(ty) - bit_modifier // 2)
-    b = (np.array(Image.open(join(input_dir, "1.png"))).astype(ty) - bit_modifier // 2)
-    image = np.vstack([a, b])
-
+def unpack(image):
     encoded_width = len(image[0])
     encoded_height = len(image) // 2
     height = encoded_height * 2
@@ -48,26 +39,53 @@ if __name__ == '__main__':
     hf_stage3 = image[:height // 8, -width * 3 // 8:]
     lf_stage3 = image[:height // 8, :width // 8]
 
-    lf_stage3_y = inverse_wavelet(np.hstack([lf_stage3, hf_stage3[:, ::3]]), np.hstack([hf_stage3[:, 1::3], hf_stage3[:, 2::3]]))
-    lf_stage2 = inverse_wavelet(lf_stage3_y.T[:width // 8], lf_stage3_y.T[width // 8:], 0).T
+    return lf_stage3, hf_stage3, hf_stage2, hf_stage1
 
-    lf_stage2_y = inverse_wavelet(np.hstack([lf_stage2, hf_stage2[:, ::3]]), np.hstack([hf_stage2[:, 1::3], hf_stage2[:, 2::3]]))
-    lf_stage1 = inverse_wavelet(lf_stage2_y.T[:width // 4], lf_stage2_y.T[width // 4:], 0).T
 
-    lf_stage1_y = inverse_wavelet(np.hstack([lf_stage1, hf_stage1[:, ::3]]), np.hstack([hf_stage1[:, 1::3], hf_stage1[:, 2::3]]))
-    lf_stage0 = inverse_wavelet(lf_stage1_y.T[:width // 2], lf_stage1_y.T[width // 2:], 0).T
+def inverse_wavelet_stage(lf, hf, pad_width=0):
+    h, w = lf.shape
+    y_transformed = inverse_wavelet(np.hstack([lf, hf[:, 0::3]]), np.hstack([hf[:, 1::3], hf[:, 2::3]]))
+    return inverse_wavelet(y_transformed.T[:w], y_transformed.T[w:], pad_width).T
+
+
+def inverse_wavelet_3(image, return_lf_parts=False):
+    lf_stage3, hf_stage3, hf_stage2, hf_stage1 = unpack(image)
+    lf_stage2 = inverse_wavelet_stage(lf_stage3, hf_stage3)
+    lf_stage1 = inverse_wavelet_stage(lf_stage2, hf_stage2)
+    decoded = inverse_wavelet_stage(lf_stage1, hf_stage1)
+    if return_lf_parts:
+        return decoded, lf_stage1, lf_stage2, lf_stage3
+    return decoded
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'usage:\n{sys.argv[0]} <input_dir>')
+        exit(1)
+    _, input_dir = sys.argv
+
+    a = (np.array(Image.open(join(input_dir, "0.png"))).astype(ty) - bit_modifier // 2)
+    b = (np.array(Image.open(join(input_dir, "1.png"))).astype(ty) - bit_modifier // 2)
+    image = np.vstack([a, b])
+
+    lf_stage3, hf_stage3, hf_stage2, hf_stage1 = unpack(image)
+    lf_stage2 = inverse_wavelet_stage(lf_stage3, hf_stage3)
+    lf_stage1 = inverse_wavelet_stage(lf_stage2, hf_stage2)
+    decoded = inverse_wavelet_stage(lf_stage1, hf_stage1)
 
     plt.figure()
     plt.title("decoded")
-    print(np.min(lf_stage0), np.max(lf_stage0))
-    plt.imshow(lf_stage0, cmap="gray", vmin=0, vmax=255)
+    print(np.min(decoded), np.max(decoded))
+    plt.imshow(decoded, cmap="gray", vmin=0, vmax=255)
+
     plt.figure()
     ref = np.array(Image.open(join(input_dir, "ref.png")))
     plt.title("ref")
     plt.imshow(ref, cmap="gray", vmin=0, vmax=255)
+
     plt.figure()
     plt.title("diff")
     crop = 16
-    plt.imshow((lf_stage0 - ref)[crop:-crop, crop:-crop], cmap="bwr", vmin=-2, vmax=+2)
+    plt.imshow((decoded - ref)[crop:-crop, crop:-crop], cmap="bwr", vmin=-2, vmax=+2)
     plt.colorbar()
     plt.show()
--- src/lib/video/wavelet_py_compressor.py
+++ src/lib/video/wavelet_py_compressor.py
@@ -3,6 +3,9 @@ import sys
 from PIL import Image
 import os
 from os.path import join
+import matplotlib.pyplot as plt
+
+from lib.video.wavelet_decompressor import inverse_wavelet_3
 
 bit_modifier = 65535
 ty = np.int16
@@ -10,25 +13,22 @@ save_ty = np.uint16
 
 
 def wavelet1d(image):
-    def lf(i):
-        k = np.roll(image, 2 * i, 0)
-        return (k[0::2] + k[1::2])
+    def px(i):
+        k = np.roll(image, -i, 0)
+        return k[::2]
 
-    lf_part = lf(0)
-    hf_part = (image[0::2] - image[1::2] + (-lf(-1) + lf(1) + 4) // 8)
+    lf_part = px(0) + px(1)
+    hf_part = (px(0) - px(1)) + (-px(-2) - px(-1) + px(2) + px(3) + 4) // 8
+
     return lf_part, hf_part
 
 
 def wavelet2d(image):
     width = len(image[0])
     lf_part, hf_part = wavelet1d(image)
-
     lf_part, hf_part = wavelet1d(np.hstack([lf_part, hf_part]).T)
-    # print(lf_part[4,4])
     lf_part = lf_part.T
     hf_part = hf_part.T
-    # plt.imshow(np.vstack([np.hstack([lf_part[:, :width // 2], hf_part[:, :width // 2]]), np.hstack([lf_part[:, width // 2:], hf_part[:, width // 2:]])]))
-    # plt.show()
     return lf_part[:, :width // 2], hf_part[:, :width // 2], lf_part[:, width // 2:], hf_part[:, width // 2:]
 
 
@@ -48,12 +48,18 @@ def full_width(w, stages):
         return w * 3 // 2 + full_width(w // 2, stages - 1)
 
 
+lf_parts = []
+
+
 def multi_stage_wavelet2d(image, stages):
     h, w = image.shape
+    global lf_parts
+    lf_parts = [image]
 
     def multi_stage_wavelet2d_impl(result, image, stages):
         h, w = image.shape
         lf_part, *hf_part = wavelet2d(image)
+        lf_parts.append(lf_part)
         if stages == 1:
             result[:h // 2] = np.hstack([lf_part, interleave(*[hf.T for hf in hf_part]).T])
         else:
@@ -75,16 +81,73 @@ if __name__ == '__main__':
         w = h = 32
         cw = ch = h // 32
         template = np.zeros((2 * ch, 2 * cw), dtype=np.uint8)
-        template[:ch, :cw] = 255
-        template[ch:, cw:] = 255
+        template[:ch, :cw] = 254
+        template[ch:, cw:] = 254
         image = np.tile(template, (h // ch, w // ch))
+    elif input_file == 'gradient':
+        w = h = 256
+        image = np.array([[(x * y) % 256 for x in range(w)] for y in range(h)]).astype(np.uint8)
+    elif input_file == 'noise':
+        image = np.random.randint(0, 255, (256, 256), np.uint8)
+    elif input_file == '127':
+        w = h = 256
+        image = np.array([[0 if y % 2 == 127 else 126] * w for y in range(h)])
     else:
         image = np.array(Image.open(sys.argv[1]))
+    image = image.astype(np.int16)
 
-    res = multi_stage_wavelet2d(image.astype(np.int16), 3)
-    h, w = res.shape
+    encoded = multi_stage_wavelet2d(image, 3)
+    h, w = encoded.shape
 
-    os.makedirs(output_dir, exist_ok=True)
-    Image.fromarray(image).save(join(output_dir, "ref.png"))
-    Image.fromarray((res[:h//2] + bit_modifier//2).astype(save_ty)).save(join(output_dir, "0.png"))
-    Image.fromarray((res[h//2:] + bit_modifier//2).astype(save_ty)).save(join(output_dir, "1.png"))
+    # os.makedirs(output_dir, exist_ok=True)
+    # Image.fromarray(image).save(join(output_dir, "ref.png"))
+    # Image.fromarray((encoded[:h // 2] + bit_modifier // 2).astype(save_ty)).save(join(output_dir, "0.png"))
+    # Image.fromarray((encoded[h // 2:] + bit_modifier // 2).astype(save_ty)).save(join(output_dir, "1.png"))
+
+    lf_decoded = inverse_wavelet_3(encoded, return_lf_parts=True)
+
+    for i, (a, b) in enumerate(zip(lf_parts, lf_decoded)):
+        crop = 16
+        a_crop = a[crop:-crop, crop:-crop]
+        b_crop = b[crop:-crop, crop:-crop]
+        diff = a_crop - b_crop
+
+        print(f"psnr {i}: {10 * np.log10(256**2 / np.sum(diff**2) * diff.size)}")
+
+        plt.figure()
+        plt.title(f"lf diff {i}")
+        plt.imshow(diff, cmap="bwr", vmin=-2, vmax=+2)
+
+        plt.figure()
+        plt.title(f"lf enc {i}")
+        plt.imshow(a_crop, cmap="gray", vmin=0, vmax=255 * 2**i)
+
+        plt.figure()
+        plt.title(f"lf dec {i}")
+        plt.imshow(b_crop, cmap="gray", vmin=0, vmax=255 * 2**i)
+
+
+        def hist(data):
+            min = np.min(data)
+            max = np.max(data)
+            plt.bar(np.arange(min, max + 1), np.bincount(np.ravel(data) - min), width=1.0)
+
+        if i == 0:
+            diff_values = a_crop[np.where(diff != 0)]
+
+            plt.figure()
+            plt.title(f"diff enc hist {i}")
+            plt.bar(range(256), np.bincount(diff_values, minlength=256), width=1.0)
+
+            plt.figure()
+            plt.title(f"diff hist {i}")
+            hist(diff)
+
+            plt.figure()
+            plt.title(f"lf hist enc {i}")
+            hist(a_crop)
+
+            plt.figure()
+            plt.title(f"lf hist dec {i}")
+            hist(b_crop)
+    plt.show()

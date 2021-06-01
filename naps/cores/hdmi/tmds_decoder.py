dac7ad68388352de7d162e375a36b1e72e169a21
Jaro Habiger <jarohabiger@googlemail.com>
2021-06-01 14:36:32 +0200

    hdmi: add tmds decoder

---
+++ naps/cores/hdmi/tmds_decoder.py
@@ -0,0 +1,41 @@
+from nmigen import *
+from .tmds import control_tokens
+from ... import iterator_with_if_elif
+
+
+class TmdsDecoder(Elaboratable):
+    """
+    Decodes tmds signals.
+    """
+    def __init__(self, input: Signal):
+        self.input = input
+
+        self.data_enable = Signal()
+        self.data = Signal(8)
+        self.control = Signal(2)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        for cond, (i, token) in iterator_with_if_elif(enumerate(control_tokens), m):
+            with cond(self.input == token):
+                m.d.comb += self.data_enable.eq(0)
+                m.d.comb += self.control.eq(i)
+        with m.Else():
+            inverted = Signal(8)
+            m.d.comb += inverted.eq(Mux(self.input[9], ~self.input[0:8], self.input[0:8]))
+            xored = Signal(8)
+            m.d.comb += xored[0].eq(inverted[0])
+            m.d.comb += xored[1].eq(inverted[1] ^ inverted[0])
+            m.d.comb += xored[2].eq(inverted[2] ^ inverted[1])
+            m.d.comb += xored[3].eq(inverted[3] ^ inverted[2])
+            m.d.comb += xored[4].eq(inverted[4] ^ inverted[3])
+            m.d.comb += xored[5].eq(inverted[5] ^ inverted[4])
+            m.d.comb += xored[6].eq(inverted[6] ^ inverted[5])
+            m.d.comb += xored[7].eq(inverted[7] ^ inverted[6])
+            with m.If(self.input[8]):  # XOR encoding
+                m.d.comb += self.data.eq(xored)
+            with m.Else():  # XNOR encoding
+                m.d.comb += self.data.eq(Cat(xored[0], ~xored[1:]))
+
+        return m
---
+++ naps/cores/hdmi/tmds_test.py
@@ -0,0 +1,59 @@
+import unittest
+import random
+from nmigen import *
+from naps import SimPlatform
+from naps.cores.hdmi.tmds import TmdsEncoder
+from naps.cores.hdmi.tmds_decoder import TmdsDecoder
+
+
+class TestTmds(unittest.TestCase):
+    def test_roundtrip(self):
+        # connect a tmds encoder to a tmds decoder and verify that we receive the characters we have sent.
+        platform = SimPlatform()
+        m = Module()
+
+        data = Signal(8)
+        control = Signal(2)
+        data_enable = Signal(8)
+        encoder = m.submodules.encoder = TmdsEncoder(data, control, data_enable)
+        decoder = m.submodules.decoder = TmdsDecoder(encoder.out)
+
+        random.seed(0)
+        test_sequence = [10, *[random.randrange(0, 255 + 4) for _ in range(0, 1000)]]
+
+        def writer():
+            for x in test_sequence:
+                if x < 256:
+                    yield data.eq(x)
+                    yield data_enable.eq(1)
+                else:
+                    yield data_enable.eq(0)
+                    yield control.eq(x >> 8)
+                yield
+        platform.add_process(writer, "sync")
+
+        def reader():
+            active = False
+            seq = [*test_sequence]
+            while len(seq):
+                x = seq[0]
+                if active:
+                    seq.pop(0)
+
+                if active:
+                    if x < 256:
+                        self.assertEqual(x, (yield data))
+                        self.assertEqual(1, (yield data_enable))
+                    else:
+                        self.assertEqual(0, (yield data_enable))
+                        self.assertEqual(x >> 8, (yield control))
+                elif (yield data) == x:
+                    active = True
+                    seq.pop(0)
+                yield
+        platform.add_process(reader, "sync")
+
+
+
+        platform.add_sim_clock("sync", 100e6)
+        platform.sim(m)

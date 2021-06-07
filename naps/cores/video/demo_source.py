a6a5810d74bf6cd518c9b489982d60a9665f7524
Jaro Habiger <jarohabiger@googlemail.com>
2021-06-07 15:13:42 +0200

    cores.video: add better demo video sources

--- naps/cores/video/demo_source.py
+++ naps/cores/video/demo_source.py
@@ -1,44 +1,75 @@
 from nmigen import *
 
 from naps import ControlSignal, StatusSignal
-from . import ImageStream
+from . import ImageStream, RGB24
 
-__all__ = ["BlinkDemoVideoSource"]
+__all__ = ["BlinkDemoVideoSource", "BertlDemoVideoSource", "SolidColorDemoVideoSource"]
 
 
-class BlinkDemoVideoSource(Elaboratable):
-    def __init__(self, payload_shape, width, height):
+class DemoVideoSource(Elaboratable):
+    def __init__(self, generator_function, payload_shape, width, height, startup_enable=True):
         self.payload_shape = payload_shape
-        self.enable = ControlSignal(reset=1)
+        self.generator_function = generator_function
+        self.enable = ControlSignal(reset=startup_enable)
         self.width = ControlSignal(16, reset=width)
         self.height = ControlSignal(16, reset=height)
         self.frame_counter = StatusSignal(32)
-        self.speed = ControlSignal(16, reset=1)
 
-        self.output = ImageStream(payload_shape, name="test_video_source_output")
+        self.output = ImageStream(payload_shape, name="demo_video_source_output")
 
     def elaborate(self, platform):
         m = Module()
 
         x_ctr = Signal(16)
         y_ctr = Signal(16)
-        m.d.comb += self.output.valid.eq(1)
-        m.d.comb += self.output.payload.eq(Repl((self.frame_counter % self.speed) > (self.speed // 2), len(self.output.payload)))
-        with m.If(self.output.ready):
-            with m.If(x_ctr < self.width):
-                m.d.sync += x_ctr.eq(x_ctr + 1)
-            with m.Else():
-                m.d.sync += x_ctr.eq(0)
-                m.d.comb += self.output.line_last.eq(1)
-                with m.If(y_ctr < self.height):
-                    m.d.sync += y_ctr.eq(y_ctr + 1)
+
+        with m.If(self.enable):
+
+            m.d.comb += self.output.valid.eq(1)
+            m.d.comb += self.output.payload.eq(
+                self.generator_function(m, self, x_ctr, y_ctr, self.frame_counter)
+            )
+            with m.If(self.output.ready):
+                with m.If(x_ctr < self.width - 1):
+                    m.d.sync += x_ctr.eq(x_ctr + 1)
                 with m.Else():
-                    m.d.sync += y_ctr.eq(0)
-                    m.d.comb += self.output.frame_last.eq(1)
-                    with m.If(self.enable):
-                        m.d.sync += self.frame_counter.eq(self.frame_counter + 1)
+                    m.d.sync += x_ctr.eq(0)
+                    m.d.comb += self.output.line_last.eq(1)
+                    with m.If(y_ctr < self.height - 1):
+                        m.d.sync += y_ctr.eq(y_ctr + 1)
                     with m.Else():
-                        m.d.sync += x_ctr.eq(x_ctr)
-                        m.d.comb += self.output.valid.eq(0)
+                        m.d.sync += y_ctr.eq(0)
+                        m.d.comb += self.output.frame_last.eq(1)
+                        m.d.sync += self.frame_counter.eq(self.frame_counter + 1)
 
         return m
+
+
+def BlinkDemoVideoSource(payload_shape, *args, **kwargs):
+    def generator_function(m, self, x, y, frame_ctr):
+        self.speed = ControlSignal(16, reset=1)
+        return Repl((frame_ctr % self.speed) > (self.speed // 2), len(self.output.payload))
+
+    return DemoVideoSource(generator_function, payload_shape, *args, **kwargs)
+
+
+def BertlDemoVideoSource(*args, **kwargs):
+    def generator_function(m, self, x, y, frame_ctr):
+        return RGB24(
+            r=x[0:8],
+            g=y[0:8],
+            b=Cat(Signal(3), y[8:10], x[8:11])
+        )
+
+    return DemoVideoSource(generator_function, 24, *args, **kwargs)
+
+
+def SolidColorDemoVideoSource(r=0, g=0, b=0, *args, **kwargs):
+    def generator_function(m, self, x, y, frame_ctr):
+        self.r = ControlSignal(8, reset=r)
+        self.g = ControlSignal(8, reset=g)
+        self.b = ControlSignal(8, reset=b)
+
+        return RGB24(r=self.r, g=self.g, b=self.b)
+
+    return DemoVideoSource(generator_function, 24, *args, **kwargs)

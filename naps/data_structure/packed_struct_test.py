97c4b88f40883d8a0af398c96f1aec9667cfecdf
Jaro Habiger <jarohabiger@googlemail.com>
2021-06-08 12:28:26 +0200

    packed_struct: add basic test for non nMigen bit packing

---
+++ naps/data_structure/packed_struct_test.py
@@ -0,0 +1,20 @@
+import unittest
+from nmigen import *
+from .packed_struct import packed_struct
+
+
+@packed_struct
+class TestStruct:
+    a: unsigned(1)
+    b: unsigned(1)
+    c: unsigned(2)
+
+
+class PackedStructTest(unittest.TestCase):
+    def test_non_nmigen(self):
+        ts = TestStruct(0b0101)
+        assert ts.a == 1
+        assert ts.b == 0
+        assert ts.c == 0b01
+
+        assert ts.fields() == ['a', 'b', 'c'], ts.fields()

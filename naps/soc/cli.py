ee764068df850996218bc6908103c061a2961aed
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-08-17 01:51:48 +0200

    soc.cli: cache multiple bitstreams and add garbage collection

--- naps/soc/cli.py
+++ naps/soc/cli.py
@@ -1,9 +1,12 @@
 import argparse
+import datetime
 import inspect
 import sys
+import hashlib
 from pathlib import Path
 from shutil import rmtree
 from textwrap import indent
+from datetime import timedelta
 
 from nmigen import Fragment
 from nmigen.build.run import LocalBuildProducts, BuildPlan
@@ -37,22 +40,56 @@ def fragment_repr(original: Fragment):
 def cli(top_class, runs_on, possible_socs=(None,)):
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--elaborate', help='Elaborates the experiment', action="store_true")
-    parser.add_argument('-b', '--build', help='builds the gateware & assembles a fatbitstream; implies -e', action="store_true")
+    parser.add_argument('-b', '--build', help='builds the gateware & assembles a fatbitstream; implies -e',
+                        action="store_true")
     parser.add_argument('--force_cache', help='forces caching of the gateware even if it changed', action="store_true")
-    parser.add_argument('-p', '--program', help='programs the board; programs the last build if used without -b', action="store_true")
+    parser.add_argument('-p', '--program', help='programs the board; programs the last build if used without -b',
+                        action="store_true")
     parser.add_argument('-r', '--run', help='run the pydriver shell after programming', action="store_true")
+    parser.add_argument('-g', '--gc', help='oldest cached bitstream to keep', type=str, default="14d",
+                        dest="gc_interval")
 
     platform_choices = {plat.__name__.replace("Platform", ""): plat for plat in runs_on}
     default = list(platform_choices.keys())[0] if len(platform_choices) == 1 else None
-    parser.add_argument('-d', '--device', help='specify the device to build for', choices=platform_choices.keys(), default=default, required=default is None)
+    parser.add_argument('-d', '--device', help='specify the device to build for', choices=platform_choices.keys(),
+                        default=default, required=default is None)
 
     default = None
     if len(possible_socs) == 1:
         default = soc_platform_name(possible_socs[0])
 
-    parser.add_argument('-s', '--soc', help='specifies the soc platform to build for', choices=list(map(soc_platform_name, possible_socs)), default=default, required=default is None)
+    parser.add_argument('-s', '--soc', help='specifies the soc platform to build for',
+                        choices=list(map(soc_platform_name, possible_socs)), default=default, required=default is None)
 
     args = parser.parse_args()
+
+    gc_age_string = args.gc_interval.strip()
+    if gc_age_string[-1] == "d":
+        gc_interval = timedelta(hours=int(gc_age_string[:-1]))
+    elif gc_age_string[-1] == "m":
+        gc_interval = timedelta(minutes=int(gc_age_string[:-1]))
+    elif gc_age_string[-1] == "s":
+        gc_interval = timedelta(seconds=int(gc_age_string[:-1]))
+    elif gc_age_string[-1] == "w":
+        gc_interval = timedelta(weeks=int(gc_age_string[:-1]))
+    else:
+        print(f"unable to parse gc interval {gc_age_string}")
+    gc_cutoff = (datetime.datetime.now() - gc_interval).timestamp()
+
+    for dir in Path("build").iterdir():
+        gateware_dir = dir / "gateware"
+        if gateware_dir.exists():
+            cache_keys = sorted(
+                [
+                    (cache_key, cache_key.stat().st_atime)
+                    for e in gateware_dir.iterdir()
+                    if e.is_dir() and (cache_key := (e / "cache_key.txt")).exists()
+                ],
+                key = lambda e: e[1],
+            )
+            for c in cache_keys[3:]:
+                if c[1] < gc_cutoff:
+                    rmtree(c[0])
 
     hardware_platform = platform_choices[args.device]
     if args.soc != 'None':
@@ -68,9 +105,6 @@ def cli(top_class, runs_on, possible_socs=(None,)):
     caller_file = Path(inspect.stack()[1].filename)
     name = caller_file.stem
     build_dir = "build" / Path(f"{name}_{args.device}_{args.soc}")
-    gateware_build_dir = build_dir / "gateware"
-    cache_key_path = build_dir / "cache_key.txt"
-    cache_key_old_path = build_dir / "cache_key_old.txt"
     fatbitstream_name = build_dir / f"{name}.zip"
 
     if not (args.program or args.build or args.elaborate or args.run):
@@ -90,6 +124,12 @@ def cli(top_class, runs_on, possible_socs=(None,)):
     if args.build:
         needs_rebuild = True
         elaborated_repr = fragment_repr(elaborated)
+
+        cache_hash = hashlib.sha256(elaborated_repr.encode()).hexdigest()
+
+        gateware_build_dir = build_dir / "gateware" / cache_hash
+        cache_key_path = gateware_build_dir / "cache_key.txt"
+
         if cache_key_path.exists():
             old_repr = cache_key_path.read_text()
             if args.force_cache:
@@ -119,9 +159,7 @@ def cli(top_class, runs_on, possible_socs=(None,)):
             timer.start_task("vendor toolchain build")
             build_products = build_plan.execute_local(gateware_build_dir)
 
-            # we write the pickle file in the end also as a marking that the build was successful
-            if cache_key_path.exists():
-                cache_key_old_path.write_text(old_repr)
+            # we write the cache key file in the end also as a marking that the build was successful
             cache_key_path.write_text(elaborated_repr)
         else:
             build_products = LocalBuildProducts(gateware_build_dir)

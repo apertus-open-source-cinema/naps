dbb9569843be7c477fbcfd7943de38854e282653
Jaro Habiger <jarohabiger@googlemail.com>
2024-01-03 11:50:20 +0100

    applets.sloth: add initial HMCAD1511 SPI controller

--- applets/sloth.py
+++ applets/sloth.py
@@ -4,10 +4,6 @@ from naps.platform.prjsloth_platform import PrjSlothPlatform
 
 
 class Top(Elaboratable):
-    def __init__(self):
-        self.width = 1280
-        self.height = 720
-
     def elaborate(self, platform: PrjSlothPlatform):
         m = Module()
 
@@ -17,17 +13,53 @@ class Top(Elaboratable):
         m.submodules.i2c = BitbangI2c(i2c_pads)
 
         power_control = platform.request("power_ctl")
-        for enable in power_control.layout:
-            name = enable[0]
-            signal = ControlSignal(name = name)
-            setattr(self, name, signal)
-            m.d.comb += power_control[name].o.eq(signal)
-        # for entry in power_control:
-        #     print(entry)
+        
+        for name, *_ in power_control.layout:
+            cs = ControlSignal(name=name)
+            setattr(self, name, cs)
+            m.d.comb += power_control[name].o.eq(cs)
 
+        m.submodules.hmcad_spi = HMCAD1511SPI()
 
         return m
+    
+class HMCAD1511SPI(Elaboratable):
+    """A bitbanged SPI write only controller for the HMCAD1511 ADC
+    """
 
+    def __init__(self) -> None:
+        self.cs = ControlSignal()
+        self.clk = ControlSignal()
+        self.copi = ControlSignal()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        adc_spi = platform.request("hmcad1511_spi")
+        m.d.comb += [
+            adc_spi.cs.o.eq(self.cs),
+            adc_spi.clk.o.eq(self.clk),
+            adc_spi.copi.o.eq(self.copi)
+        ]
+
+        return m
+    
+    @driver_method
+    def write_word(self, address: int, data: int):
+        self.cs = 0
+        self.cs = 1
+
+        for bit in reversed(range(8)):
+            self.clk = 0
+            self.copi = address >> bit & 0b1
+            self.clk = 1
+        
+        for bit in reversed(range(16)):
+            self.clk = 0
+            self.copi = data >> bit & 0b1
+            self.clk = 1
+
+        self.cs = 0
 
 if __name__ == "__main__":
     cli(Top, runs_on=(PrjSlothPlatform,), possible_socs=(ZynqSocPlatform, ))

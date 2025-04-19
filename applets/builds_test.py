265ef59f424c6a4837df6183dc1458dc2112831f
Jaro Habiger <jarohabiger@googlemail.com>
2025-04-19 02:18:55 +0200

    speed up test discovery for build tests

--- applets/builds_test.py
+++ applets/builds_test.py
@@ -3,20 +3,21 @@ import subprocess
 import unittest
 from glob import glob
 from os.path import dirname
+from typing import Tuple
 
 
 class TestBuilds(unittest.TestCase):
     pass
 
-
+process_handles: list[Tuple[str, subprocess.Popen]] = []
 for file in glob("{}/*.py".format(dirname(__file__))):
     if "builds_test" in file:  # we are not executing ourselves
         continue
-    try:
-        subprocess.check_output(['python', file], stderr=subprocess.PIPE)
-        continue
-    except subprocess.CalledProcessError as e:
-        usage = e.stderr.decode("utf-8")
+    process_handles.append((file, subprocess.Popen(['python', file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)))
+
+for file, p in process_handles:
+    stdout, stderr = p.communicate()
+    usage = stderr.decode("utf-8")
     try:
         devices = re.search("-d\\W*{(.*?)}", usage).group(1).split(",")
         socs = re.search("-s\\W*{(.*?)}", usage).group(1).split(",")
